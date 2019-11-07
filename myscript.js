d4cbc3f4cddaafebf568ad574a4e1fae1b6fefb8
rmr1154 <38836662+rmr1154@users.noreply.github.com>
2019-11-07 14:25:10 -0500

    css sux

--- myscript.js
+++ myscript.js
@@ -1,16 +1,25 @@
-    $(function () {
-            setNavigation();
-        });
-        
-        function setNavigation() {
-            var path = window.location.pathname;
-            path = path.replace(/\/$/, "");
-            path = decodeURIComponent(path);
-        
-            $(".nav a").each(function () {
-                var href = $(this).attr('href');
-                if (path.substring(0, href.length) === href) {
-                    $(this).closest('li').addClass('active');
-                }
-            });
-        }
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFuncMnu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFuncDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
